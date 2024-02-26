6e626876b3ae4365b4aca4a88684e779866d387e
unknown <enzo2005cabrera@gmail.com>
2024-02-26 20:58:40 -0300

    Class one

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
 console.log("Hello");
+var x = 1;
+var y = 2.1;
+
+var day = "segunda";
+
+console.log("O valor de x: é" + x);
+console.log("O valor de y: é" + y);
+console.log("Hoje é dia: é" + day);
+var z = x * y;
+console.log("A multiplicação é " + z);
+
+var z = x * y + day;
+console.log("A multiplicação é " + z);
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof day);
+
+const pessoa = {
+    nome: 'Enzo',
+    profissao: 'Suporte',
+    idade: 19
+}
+
+console.log(pessoa);
+
+const aviao = {
+    modelo: 'Boeing 777',
+    cor: 'vermelhor',
+    motor: 'V12'
+}
+console.log(aviao);
+
+function ola() {
+    console.log('function');
+}
+ola();
+
+const segundaFuncao = () => {
+    console.log('Olá')
+}
+
+function funcaoComParametro(nome, sobrenome) {
+    return `olá[1] ${nome} ${sobrenome}`;
+}
+funcaoComParametro()
+
+for(let i = 0; i < 10; i++)
+    console.log(`Numero ${i}`);
